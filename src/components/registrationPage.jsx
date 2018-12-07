d12911652c9607ad9ce563f1ffd74e93044decdb
Yakim Rachev <jakimcho@gmail.com>
2018-12-07 16:11:47 +0200

    Added controlled registration form inputs

--- src/components/registrationPage.jsx
+++ src/components/registrationPage.jsx
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 
 class RegistrationPage extends Component {
-  state = {};
+  state = {
+    user: {
+      email: "",
+      firstName: "",
+      sirName: "",
+      password: "",
+      confirmPassword: "",
+      city: "",
+      country: "Bulgaria",
+      zip: "",
+      address: ""
+    }
+  };
 
-  username = React.createRef;
+  username = React.createRef();
 
   handleSubmit = e => {
     e.preventDefault();
 
-    console.log("Sending data with user name: ", this.username.current.value);
+    console.log(
+      "Proba Sending data with user name: ",
+      this.username.current.value
+    );
+  };
+
+  handleEmailChange = e => {
+    const user = { ...this.state.user };
+    user.email = e.currentTarget.value;
+    this.setState({ user });
+  };
+
+  // destructuring the e (event object) and get currentTarget as alias input
+  handleAllFieldsChange = ({ currentTarget: input }) => {
+    const user = { ...this.state.user };
+    user[input.name] = input.value;
+    this.setState({ user });
   };
 
   render() {
+    const { user } = this.state;
     return (
       <div className="cotainer mt-4">
         <div className="row justify-content-center">
@@ -27,9 +56,12 @@ class RegistrationPage extends Component {
                     <div className="form-group col-md-8">
                       <label htmlFor="inputEmail4">Email</label>
                       <input
+                        value={user.email}
+                        onChange={this.handleEmailChange}
                         type="email"
+                        name="email"
                         className="form-control"
-                        id="inputEmail4"
+                        id="email"
                         placeholder="Email"
                         ref={this.username}
                       />
@@ -40,7 +72,10 @@ class RegistrationPage extends Component {
                     <div className="form-group col-md-4">
                       <label htmlFor="inputPassword4">Password</label>
                       <input
+                        value={user.password}
+                        onChange={this.handleAllFieldsChange}
                         type="password"
+                        name="password"
                         className="form-control"
                         id="inputPassword4"
                         placeholder="Password"
@@ -49,7 +84,10 @@ class RegistrationPage extends Component {
                     <div className="form-group col-md-4">
                       <label htmlFor="confirmPassword">Confirm Password</label>
                       <input
+                        value={user.confirmPassword}
+                        onChange={this.handleAllFieldsChange}
                         type="password"
+                        name="confirmPassword"
                         className="form-control"
                         id="confirmPassword"
                         placeholder="Confirm Password"
@@ -62,17 +100,23 @@ class RegistrationPage extends Component {
                       <label htmlFor="userFirstName">First Name</label>
                       <input
                         type="text"
+                        value={user.firstName}
+                        onChange={this.handleAllFieldsChange}
                         className="form-control"
                         id="userFirstName"
+                        name="firstName"
                         placeholder="First Name"
                       />
                     </div>
                     <div className="form-group col-md-4">
                       <label htmlFor="userSirName">Sir Name</label>
                       <input
+                        value={user.sirName}
+                        onChange={this.handleAllFieldsChange}
                         type="text"
                         className="form-control"
                         id="userSirName"
+                        name="sirName"
                         placeholder="Sir Name"
                       />
                     </div>
@@ -82,22 +126,32 @@ class RegistrationPage extends Component {
                     <div className="form-group col-md-3">
                       <label htmlFor="inputCity">City</label>
                       <input
+                        value={user.city}
+                        onChange={this.handleAllFieldsChange}
                         type="text"
+                        name="city"
                         className="form-control"
                         id="inputCity"
                       />
                     </div>
                     <div className="form-group col-md-3">
                       <label htmlFor="inputState">State</label>
-                      <select id="inputState" className="form-control">
-                        <option selected>Choose...</option>
+                      <select
+                        //value={user.country}
+                        id="inputState"
+                        className="form-control"
+                      >
+                        <option defaultValue>Choose...</option>
                         <option>...</option>
                       </select>
                     </div>
                     <div className="form-group col-md-2">
                       <label htmlFor="inputZip">Zip</label>
                       <input
+                        value={user.zip}
+                        onChange={this.handleAllFieldsChange}
                         type="text"
+                        name="zip"
                         className="form-control"
                         id="inputZip"
                       />
@@ -109,8 +163,11 @@ class RegistrationPage extends Component {
                       <label htmlFor="inputAddress">Address</label>
                       <input
                         type="text"
+                        value={user.address}
+                        onChange={this.handleAllFieldsChange}
                         className="form-control"
                         id="inputAddress"
+                        name="address"
                         placeholder="1234 Main St"
                       />
                     </div>
